5d25b7a312d960fd445ee0427409d4766cc3abbb
Eduardo Vencovsky <eduvencovsky@gmail.com>
2019-02-26 14:27:02 -0300

    simple crud

--- server.js
+++ server.js
@@ -1,15 +1,56 @@
 const express = require('express')
+const bodyParser = require('body-parser')
+const _ = require('lodash')
 
 const app = express()
 
-const jsonData = {count: 312, message: 'hey'}
+app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.json())
 
-app.get('/', (req, res) => {
-    res.json(jsonData)
+const lions = [{oi: 'oi'}]
+const id = 0
+
+app.get('/lions', (req, res) => {
+    res.json(lions)
 })
 
-app.get('/data', (req, res) => {
-    res.json(jsonData)
+app.get('/lions/:id', (req, res) => {
+    const lion = _.find(lions, { id: req.params.id })
+    res.json(lion || {})
+})
+
+app.post('/lions', (req, res) =>  {
+    const lion = req.body
+    id++ 
+    lion.id = id.toString()
+    lions.push(lion)
+    res.json(lion)
+})
+
+app.put('/lions/:id', (req, res) => {
+    let update = req.body
+
+    if(update.id) delete update.id
+
+    const lion = _.findIndex(lions, { id: req.params.id })
+
+    if(!lions[lion]){
+        res.send()
+    } else {
+        let updatedLion = _.assign(lions[lion], update)
+        res.json(updatedLion)
+    }
+})
+
+app.delete('/lions/:id', (req, res) => {
+    const lion = _.findIndex(lions, { id: req.params.id })
+    if(!lions[lion]){
+        res.send()
+    } else {
+        const deletedLion = lions[lion] 
+        lions.splice(lion, 1)
+        res.json(deletedLion)
+    }
 })
 
 const port = 3000
